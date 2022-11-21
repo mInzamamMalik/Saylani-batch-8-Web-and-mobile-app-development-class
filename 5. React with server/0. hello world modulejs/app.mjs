8e8b87c65ed87492c37f8827c0942c71eee57b3a
M.Inzamam Malik <malikasinger1@gmail.com>
2022-11-22 00:31:51 +0500

    class code

---
+++ 5. React with server/0. hello world modulejs/app.mjs
@@ -0,0 +1,7 @@
+console.log("hello world es module javascript");
+
+let a = 5;
+
+if (a === 5) {
+    console.log("condition is true");
+}
---
+++ 5. React with server/1. hello world server/server.mjs
@@ -0,0 +1,14 @@
+console.log("I am server file");
+
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
+})
+// http://172.16.23.138:3000
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
