c740d3b2c1cae4775e018636057923163d75cec3
M.Inzamam Malik <malikasinger1@gmail.com>
2022-11-25 22:33:22 +0500

    single page hosting technique

--- 5. React with server/3. static hosting with express/server.mjs
+++ 5. React with server/3. static hosting with express/server.mjs
@@ -8,10 +8,18 @@ app.get('/abc', (req, res) => {
     console.log("request ip: ", req.ip);
     res.send('Hello World! ' + new Date().toString());
 })
+app.get('/getweather', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
+})
+app.get('/gettime', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
+})
 
 const __dirname = path.resolve();
 app.use('/', express.static(path.join(__dirname, './web/build')))
-
+app.get('*', express.static(path.join(__dirname, './web/build')))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
