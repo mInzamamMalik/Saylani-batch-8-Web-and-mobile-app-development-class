a8e7605b36c59a03ac001a4a9c3585c8cd9ef692
M.Inzamam Malik <malikasinger1@gmail.com>
2022-11-25 22:21:08 +0500

    class code

---
+++ 5. React with server/3. static hosting with express/server.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/abc', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
+})
+
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname, './web/build')))
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ 5. React with server/3. static hosting with express/web/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+      <h1>I am hello world react front end</h1>
+    </div>
+  );
+}
+
+export default App;
