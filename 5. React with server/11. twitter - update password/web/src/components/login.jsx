ceb05d89497cdafb55e202eb1e5a39b1b3b7f3f6
M.Inzamam Malik <malikasinger1@gmail.com>
2023-01-06 21:34:42 +0500

    new folder

---
+++ 5. React with server/11. twitter - update password/web/src/components/login.jsx
@@ -0,0 +1,91 @@
+import { useState, useContext } from "react";
+import { GlobalContext } from '../context/Context';
+
+import { Button, TextField } from '@mui/material';
+
+import './login.css'
+import axios from "axios";
+
+
+function Login() {
+    let { state, dispatch } = useContext(GlobalContext);
+
+    const [result, setResult] = useState("");
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginHandler = async (e) => {
+        e.preventDefault();
+
+        try {
+            let response = await axios.post(`${state.baseUrl}/login`, {
+                email: email,
+                password: password
+            }, {
+                withCredentials: true
+            })
+
+            dispatch({
+                type: 'USER_LOGIN',
+                payload: response.data.profile
+            })
+
+
+            console.log("login successful");
+            setResult("login successful")
+
+        } catch (e) {
+            console.log("e: ", e);
+        }
+
+        // e.reset();
+    }
+
+
+    return (
+        <>
+            <h4>This is Login page</h4>
+
+            <form onSubmit={loginHandler} className="loginForm">
+
+
+                <TextField
+                    className="TextField"
+                    id="email"
+                    label="Email"
+                    variant="outlined"
+                    type="email"
+                    name="username"
+                    placeholder="email"
+                    autoComplete="username"
+                    onChange={(e) => { setEmail(e.target.value) }}
+                />
+
+
+                <br />
+
+                <TextField
+                    className="TextField"
+                    id="password"
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    name="current-password"
+                    autoComplete="current-password"
+                    placeholder="password"
+                    onChange={(e) => { setPassword(e.target.value) }}
+                />
+
+                <br />
+                <Button variant="outlined" type="submit">Login</Button>
+
+            </form>
+
+
+            <p>{result}</p>
+        </>
+    )
+}
+
+export default Login;
---
+++ 5. React with server/11. twitter - update password/web/src/context/Context.js
@@ -0,0 +1,22 @@
+import React, { createContext, useReducer } from 'react'
+import { reducer } from './Reducer';
+
+export const GlobalContext = createContext("Initial Value");
+
+let data = {
+  darkTheme: true,
+  user: {},
+  isLogin: null,
+  baseUrl: (window.location.href.includes('localhost'))
+    ?
+    `http://localhost:5001/api/v1` : `/api/v1`
+}
+
+export default function ContextProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, data)
+  return (
+    <GlobalContext.Provider value={{ state, dispatch }}>
+      {children}
+    </GlobalContext.Provider>
+  )
+}
